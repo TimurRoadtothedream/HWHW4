e63d4120f273a3a8f9e1ef4b4d2ed3afe08d0fbc
Timur <xanov.timur@mail.ru>
2023-06-27 20:12:39 +0300

    Сделали 2 задачи

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+int n = 0;
+
+while (N > 0)
+{
+    n = N % 10;
+    sum = sum + n;
+    N = N/10;
+}
+Console.WriteLine($"Сумма всех чисел равна: {sum}");
