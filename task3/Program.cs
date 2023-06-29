e970ea3f8934a86d5805020d511de1f52ae3ee8d
Timur <xanov.timur@mail.ru>
2023-06-29 20:47:53 +0300

    Добавили остальное

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //Console.WriteLine("Hello, World!");
-int[] N = new int[5] {1, 2, 5, 7, 19};
+int[] array = new int[5] {1, 2, 5, 7, 19};
+
+for (int i = 0; i < 5; i++)
+
 {
-    Console.Write(N);
+    Console.Write(array[i] + "; ");
 }
